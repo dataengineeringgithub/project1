ce368819c19e83004baa05827c7ef47a25fad25d
dataengineeringgithub <ma475@duke.edu>
2021-09-10 00:21:12 +0000

    my app

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
-from fastapi import FastAPI
-import uvicorn
-import math
-import phonenumbers
-from phonenumbers import geocoder
-from phonenumbers import carrier
+from flask import Flask, render_template, request
+import requests
 
-app = FastAPI()
 
-@app.get("/")
-def root():
-    return "Welcome!"
 
-@app.get("/get-item/{phone_number}")
-def get_item(phone_number: str):
-    coun = phonenumbers.parse(phone_number, "CH")
+def get_weather_results(zip_code):
+    api_key = "bd04540de70a58837a4af62931553235"
+    api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
+    r = requests.get(api_url)
+    return r.json()
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def weather_dashboard():
+    return render_template("tem.html")
+
+@app.route('/results', methods ={'POST'})
+def render_results():
+    zip_code = request.form['zipCode']
     
-    country = geocoder.description_for_number(coun, "en")
-    
-    
-    return country
-
+    data = get_weather_results(zip_code)
+    temp = "{0:.2f}".format(data["main"]["temp"])
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+    return render_template('tem2.html', 
+                            location=location, 
+                            temp=temp, 
+                            feels_like=feels_like, 
+                            weather=weather)
+  
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8080, host ='0.0.0.0')
-
-
-
-    
-    
+    app.run()    
 
     
     
