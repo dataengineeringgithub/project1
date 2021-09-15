3e9c9ac4a4ef8dab914cba78d1c36be90a78b92e
Mohammad Anas <90017409+dataengineeringgithub@users.noreply.github.com>
2021-09-15 15:26:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request
 import requests
 
 
-def sum(a, b):
-    return a+b
-
 
 def get_weather_results(zip_code):
     api_key = "bd04540de70a58837a4af62931553235"
